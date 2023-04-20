a629a7a52a64f67d0b4854154c395de1f1c7f32f
Arya Reais-Parsi <aryap@berkeley.edu>
2023-04-20 02:18:21 -0700

    pay script last used to pay in $4.20 and $0.69 increments

---
+++ pay.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# Please excuse gratuitous biz-ing in this file.
+
+import sys
+import random
+
+import venmo_api
+from emoji import emojize
+
+# Use the the get_access_token call with your password to get an access token.
+#access_token = venmo_api.Client.get_access_token(username='himself@aryaparsi.com', password='')
+#print(access_token)
+#sys.exit(1)
+access_token = 'put your access token here'
+
+client = venmo_api.Client(access_token=access_token)
+#users = client.user.search_for_users(query='@calskiclub', username=True)
+#for user in users:
+#    print(user.username)
+#user = client.user.get_user_by_username('@aryareaisparsi')
+#print(user)
+
+profile = client.user.get_my_profile()
+print(profile)
+
+# get cal ski club id from transactions already made to them
+#transactions = client.user.get_user_transactions(user_id=profile.id)
+#for transaction in transactions:
+#    print(transaction) 
+
+# Greatest ski club of all time
+calskiclub_id = '3354877237395456471'
+
+payment_methods_by_id = {}
+payment_methods = client.payment.get_payment_methods()
+for method in payment_methods:
+    payment_methods_by_id[method.id] = method
+
+payment_id = '2322111683100672382'
+payment_method = payment_methods_by_id[payment_id]
+assert(isinstance(payment_method, venmo_api.models.payment_method.BankAccount))
+
+print(f'using payment method: {payment_method}')
+
+# emoji test
+sunglasses = u'\U0001F60E'
+pleading_face = u'\U0001F97A'
+loveheart = emojize(':red_heart:')
+#sunglasses2 = emojize(':smiling_face_with_sunglasses:')
+#with open('/tmp/test.txt', 'w') as f:
+#    f.write(f'hello {sunglasses}')
+#    f.write(sunglasses2)
+
+message = ''''''
+words = message.split()
+print(len(words))
+
+payments = [4.20] * 8 + [0.69] * 3
+random.shuffle(payments)
+
+payments = payments + [0.69]
+
+print(sum(payments))
+
+#num_payments = len(payments)
+#for i in range(0, num_payments):
+#    word = 'cssc forever'
+#    payment = payments[i]
+#    note = f'{loveheart}  {word} {loveheart}  [{i + 1}/{num_payments}]'
+#    print(f'payment {i}: {payment} {note}')
+#    client.payment.send_money(amount=payment,
+#                              note=note,
+#                              target_user_id=calskiclub_id,
+#                              funding_source_id=payment_id)
+
+client.log_out(f'Bearer {access_token}')
